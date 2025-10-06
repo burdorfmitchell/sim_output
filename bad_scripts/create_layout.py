d75c9ade453a4f1f58b85d2bc2e5c9bce6145cb9
Mitchell Burdorf <burdorfmitchell@gmail.com>
2025-10-06 13:40:51 -0400

    updates

--- bad_scripts/create_layout.py
+++ bad_scripts/create_layout.py
@@ -2,7 +2,7 @@ import copy
 
 import numpy as np
 
-fname = "L_array.csv"
+fname = "L_array"
 
 # creating an Nx3 of antenna ENUs
 
@@ -19,11 +19,19 @@ layout = np.zeros(shape=(50,3))
 layout[:25,0] = ant_locs
 layout[25:,1] = ant_locs
 
+# rotating array by theta degrees
+theta = np.pi/4
+R = [[np.cos(theta), -np.sin(theta)],
+     [np.sin(theta),  np.cos(theta)]]
+
+# rotating the xy portion by theta degrees
+layout[:,[0,1]] = np.asarray([R @ bi for bi in layout[:,[0,1]]])
+
 header = "Name\tNumber\tBeamID\tE\tN\tU\n\n"
 
-with open(fname, "w") as f:
+with open(fname+str(int(theta * 180 / np.pi))+".csv", "w") as f:
     f.write(header)
     for i, enu in enumerate(layout):
         f.write(
-            f"ANT{int(i):02d}\t{i}\t0\t{enu[0]:.1f}\t{enu[1]:.1f}\t{enu[2]:.1f}\n"
+            f"ANT{int(i):02d}\t{i}\t0\t{enu[0]:.2f}\t{enu[1]:.2f}\t{enu[2]:.2f}\n"
         )
